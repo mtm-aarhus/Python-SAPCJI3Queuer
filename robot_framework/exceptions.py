bb8e6873c2cbf65985452731d3ef94c205994c71
jduust <36190025+jduust@users.noreply.github.com>
2026-09-11 10:18:42 +0200

    error handling

--- robot_framework/exceptions.py
+++ robot_framework/exceptions.py
@@ -25,7 +25,8 @@ def handle_error(message: str, error: Exception, queue_element: QueueElement | N
         queue_element: The queue element to fail, if any.
         orchestrator_connection: A connection to OpenOrchestrator.
     """
-    error_msg = f"{message}: {repr(error)}\n\nTrace:\n{traceback.format_exc()}"
+    raw_msg = f"{message}: {repr(error)}\n\nTrace:\n{traceback.format_exc()}"
+    error_msg = f"{raw_msg[:497]}\n...\n{raw_msg[-497:]}" if len(raw_msg) > 999 else raw_msg
     error_email = orchestrator_connection.get_constant(config.ERROR_EMAIL).value
 
     orchestrator_connection.log_error(error_msg)
